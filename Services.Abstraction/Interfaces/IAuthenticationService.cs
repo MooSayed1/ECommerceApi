dee2b77a523954674738e28759dabc75d64f0ccf
Mohamed <mohamed1212213183@gmail.com>
2025-08-20 06:42:04 +0300

    try to solve conflict

--- Services.Abstraction/Interfaces/IAuthenticationService.cs
+++ Services.Abstraction/Interfaces/IAuthenticationService.cs
@@ -1,10 +1,12 @@
+using Domain.Entities;
 using Shared.Dtos.User;
 
-namespace Services.Abstraction;
+namespace Services.Abstraction.Interfaces;
 
 public interface IAuthenticationService
 {
     // email , DisplayName, Token
     public Task<UserResultDto>LoginAsync(LoginDto loginDto);
     public Task<UserResultDto> RegisterAsync(RegisterDto registerDto);
+    public Task<string> CreateTokenAsync(User user);
 }
